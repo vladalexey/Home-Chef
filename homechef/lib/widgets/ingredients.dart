f18d5ef1a11a6e94b9d59072caa108d4121f78e5
Phan Quân <qmp23@drexel.edu>
2020-03-26 17:30:35 -0400

    Add stateless widgets ingredients and instructions for recipe page

---
+++ homechef/lib/widgets/ingredients.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Ingredients extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
