19ab8ce417bdfcccecb1607cabc059a975a16ffb
Phan Quân <qmp23@drexel.edu>
2020-04-03 18:09:02 -0400

    Change UI home

--- homechef/lib/screens/home.dart
+++ homechef/lib/screens/home.dart
@@ -21,6 +21,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  GlobalKey topPartKey = GlobalKey();
   double _searchBarWidth;
 
   Widget buildGradientTopCarousel() {
@@ -28,60 +29,52 @@ class _MyHomePageState extends State<MyHomePage> {
       padding: EdgeInsets.only(
         left: 20.0,
         right: 20.0,
-        top: 70.0,
+        top: 30.0,
+        bottom: 5.0
       ),
       
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
         mainAxisSize: MainAxisSize.max,
 
         children: <Widget>[
 
-          SizedBox(height: 
-            MediaQuery.of(context).size.width * 0.4
-          ),
-
-          Column(
+          Row(
             children: <Widget>[
               Padding(
-                padding: const EdgeInsets.only(left: 8.0),
-                child: Align(
-                  alignment: Alignment.topLeft,
-                  child: SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.5,
-                    child: Divider(
-                      height: 2.0,
-                      // color: Colors.yellow[700],
-                      // color: Color(0xff383838),
-                      color: Colors.yellow[500],
-                      thickness: 3.0,
-                    ),
+                padding: const EdgeInsets.only(left: 8.0, bottom: 8.0),
+                child: Text(
+                  'Most trending',
+                  style: TextStyle(
+                    shadows: [
+                      Shadow(
+                        color: Colors.black26,
+                        offset: Offset(0.0, 2.0),
+                        blurRadius: 6.0,
+                      ),
+                    ],
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.black87,
                   ),
                 ),
-              ),
-              Row(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(left: 8.0, bottom: 20.0),
-                    child: Text(
-                      'Most popular',
-                      style: TextStyle(
-                        shadows: [
-                          Shadow(
-                            color: Colors.black26,
-                            offset: Offset(0.0, 2.0),
-                            blurRadius: 6.0,
-                          ),
-                        ],
-                        fontSize: 30.0,
-                        fontWeight: FontWeight.w600,
-                        color: Colors.black87,
-                      ),
-                    ),
-                  )
-                ],
-              ),
+              )
             ],
+          ),
+
+          Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width * 0.5,
+                child: Divider(
+                  height: 2.0,
+                  color: Colors.yellow[500],
+                  thickness: 3.0,
+                ),
+              ),
+            ),
           )
         ],
       ),
@@ -104,86 +97,78 @@ class _MyHomePageState extends State<MyHomePage> {
         );
       },
       
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(40.0),
-          color: Colors.black54,
-        ),
-        height: 60.0,
-        width: _searchBarWidth,
-        child: Align(
-          alignment: Alignment.centerRight,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 10.0,
-              vertical: 2.0
-              ),
+      child: Padding(
+        padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.2),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30.0),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black45,
+                offset: Offset(3, 5),
+                blurRadius: 20,
+                spreadRadius: 3
+              )
+            ],
+            color: Colors.yellow[200],
+            
+          ),
+          height: 60.0,
+          width: 60.0,
+          child: Align(
+            alignment: Alignment.center,
             child: Icon(Icons.search,
               size: 30.0,
-              color: Colors.white)
-              ),
+              color: Colors.black87),
+            ),
           ),
-        )
+      )
 
     );
+  }
+
+  double getPosTopPart() {
+    RenderBox topPart = topPartKey.currentContext.findRenderObject();
+    return topPart.localToGlobal(Offset.zero).dx;
   }
 
   @override
   Widget build(BuildContext context) {
 
-    _searchBarWidth = MediaQuery.of(context).size.width * 0.8;
+    _searchBarWidth = MediaQuery.of(context).size.width * 0.2;
 
     return DefaultTabController (
 
       length:3,
       child: new Scaffold(
+        
+        floatingActionButton: searchBarHome(),
+        floatingActionButtonLocation: FloatingActionButtonLocation.endTop,
 
         body: TabBarView(
           children: <Widget>[
             
-            Stack(
-              children: <Widget>[
+            SafeArea(
+              child: Stack(
+                children: <Widget>[
+                  ListView(
+                    children: <Widget>[
+                      
+                      buildGradientTopCarousel(),
 
-                Column(
-
-                  mainAxisSize: MainAxisSize.max,
-                  children: <Widget>[
-
-                    Stack(
-                      children: <Widget>[
-
-                        PopularCarouselPage(),
-                        buildGradientTopCarousel(),
-
-                        Align(
-                          alignment: Alignment.topCenter,
-                          child: AnimatedContainer(
-                            duration: Duration(milliseconds: 200),
-                            width: _searchBarWidth,
-                            child: Padding(
-                              padding: const EdgeInsets.only(top: 50.0),
-                              child: searchBarHome()),
-                          ),
-                        ),
-                        
-                      ],
-                    ),
-
-                    Expanded(
-                          child: ListView(
-                            children: <Widget>[
-                              CuisineCarousel(),
-                              SizedBox(height: 5.0),
-                              DietCarousel(),
-                              SizedBox(height: 5.0),
-                              TimeCarousel()
-                            ],
-                          ),
-                        ),
-                  ],
-                ),
-                
-              ],
+                      PopularCarouselPage(),
+                      SizedBox(height: 15.0),
+                      CuisineCarousel(),
+                      SizedBox(height: 5.0),
+                      DietCarousel(),
+                      SizedBox(height: 5.0),
+                      TimeCarousel(),
+                      
+                    ],
+                  ),
+                  
+                ],
+              ),
             ),
 
             new Container(
