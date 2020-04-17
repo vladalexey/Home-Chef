e9dc161b6101c6d45a98caafaaefe9fefc346e2a
Phan Quân <qmp23@drexel.edu>
2020-04-17 18:03:02 -0400

    Add colors for rating stars and change to Icon

--- homechef/lib/widgets/rating_stars.dart
+++ homechef/lib/widgets/rating_stars.dart
@@ -3,21 +3,32 @@ import 'package:flutter/material.dart';
 class RatingStars extends StatelessWidget {
 
   final int rating;
-  RatingStars({@required this.rating});
+  final Color color;
+  RatingStars({@required this.rating, this.color});
 
-  Text _buildRatingStars(int rating) {
+  Row _buildRatingStars(int rating, Color color) {
+
+    Row stars = Row(
+      children: <Widget>[
+      for (int i = 0; i < 5; i++) (i < rating) 
+        ? Icon(Icons.star, color: this.color, size: 16.0,)
+        : Icon(Icons.star_border, color: this.color, size: 16.0,)
+    ],);
+
+
+    return stars;
     
-    String stars = '';
-    for (int i = 0; i < rating; i++) {
-      stars += '⭐ ';
-    }
-    stars.trim();
-    return Text(stars, style: TextStyle(fontSize: 18.0),);
+    // String stars = '';
+    // for (int i = 0; i < rating; i++) {
+    //   stars += '⭐ ';
+    // }
+    // stars.trim();
+    // return Text(stars, style: TextStyle(fontSize: 18.0),);
   }
 
   @override
   Widget build(BuildContext context) {
-    return _buildRatingStars(this.rating);
+    return _buildRatingStars(this.rating, this.color);
   }
 }
 
