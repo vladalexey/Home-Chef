f2500822eab6f3e8342f5884a5e29a1fed03bb88
Phan Quân <qmp23@drexel.edu>
2020-04-19 10:55:47 -0400

    Add counter for active filters

---
+++ homechef/lib/widgets/search_widgets/filter_pages/count_active_filter.dart
@@ -0,0 +1,11 @@
+import 'package:homechef/models/cuisine_model.dart';
+import 'package:homechef/models/diet_model.dart';
+
+int numFilters() {
+  int numFilter = 0;
+
+  for (String key in cuisine_list.keys) if (cuisine_list[key]) numFilter += 1;
+  for (String key in dietOptions.keys) if (dietOptions[key]) numFilter += 1;
+
+  return numFilter;
+}
