103ebcb3a6f6f10513aadd46e5e11d3884da2b4d
Phan Quân <qmp23@drexel.edu>
2020-04-19 10:58:35 -0400

    Replace new search screen

--- homechef/lib/screens/search_screen.dart
+++ homechef/lib/screens/search_screen.dart
@@ -1,3 +1,8 @@
+import 'dart:async';
+
+import 'package:homechef/BLoC/bloc_filter_noti.dart';
+import 'package:homechef/BLoC/bloc_provider.dart';
+
 import '../widgets/flappy_search_bar-1.7.2-modified/lib/flappy_search_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:homechef/models/recipe_model.dart';
@@ -7,13 +12,12 @@ import 'package:homechef/widgets/search_widgets/search_bar.dart';
 import 'package:solid_bottom_sheet/solid_bottom_sheet.dart';
 
 class SearchScreen extends StatefulWidget {
-
   @override
   _SearchScreenState createState() => _SearchScreenState();
 }
 
-class _SearchScreenState extends State<SearchScreen> with SingleTickerProviderStateMixin {
-  
+class _SearchScreenState extends State<SearchScreen>
+    with SingleTickerProviderStateMixin {
   SolidController _solidController;
   SearchBarController<Recipe> _searchController;
 
@@ -25,44 +29,46 @@ class _SearchScreenState extends State<SearchScreen> with SingleTickerProviderSt
 
   @override
   Widget build(BuildContext context) {
+    return BlocProvider<NotiFilterBloc>(
+      bloc: NotiFilterBloc(),
 
-    return Scaffold(
-      body: Column(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              SizedBox(width: 10.0, height: 100.0,),
-              IconButton(
-                onPressed: () => Navigator.pop(context), 
-                icon: Icon(Icons.arrow_back_ios))
-            ],
-          ),
-
-          Flexible(
-            child: Padding(
-              padding: const EdgeInsets.only(top: 0.0),
-              child: SearchBarWidget(
-                searchController: _searchController,
+      child: Scaffold(
+        body: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(
+                  width: 10.0,
+                  height: 100.0,
+                ),
+                IconButton(
+                    onPressed: () => Navigator.pop(context),
+                    icon: Icon(Icons.arrow_back_ios))
+              ],
+            ),
+            Flexible(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 0.0),
+                child: SearchBarWidget(
+                  searchController: _searchController,
+                ),
               ),
             ),
-          ),
-
-        ],
-      ),
-      bottomSheet: SolidBottomSheet(
-        onHide: () {
-           _searchController.triggerSearch();
-          },
-        toggleVisibilityOnTap: false,
-        maxHeight: MediaQuery.of(context).size.height * 0.65  ,
-        controller: _solidController,
-        draggableBody: true,
-        headerBar: BottomSheetHeader(),
-        body: BottomSheetBuilder()
+          ],
+        ),
+        bottomSheet: SolidBottomSheet(
+            onHide: () {
+              _searchController.triggerSearch();
+            },
+            toggleVisibilityOnTap: false,
+            maxHeight: MediaQuery.of(context).size.height * 0.65,
+            controller: _solidController,
+            draggableBody: true,
+            headerBar: BottomSheetHeader(),
+            body: BottomSheetBuilder()),
       ),
     );
   }
