32d6f0545d0da5b2989df2ebed382083a92df19b
Phan Quân <qmp23@drexel.edu>
2020-04-20 00:30:34 -0400

    Add time option into notification counter

--- homechef/lib/widgets/search_widgets/filter_pages/count_active_filter.dart
+++ homechef/lib/widgets/search_widgets/filter_pages/count_active_filter.dart
@@ -1,11 +1,13 @@
 import 'package:homechef/models/cuisine_model.dart';
 import 'package:homechef/models/diet_model.dart';
+import 'package:homechef/models/time_model.dart';
 
 int numFilters() {
   int numFilter = 0;
 
   for (String key in cuisine_list.keys) if (cuisine_list[key]) numFilter += 1;
   for (String key in dietOptions.keys) if (dietOptions[key]) numFilter += 1;
+  for (String key in timesOptions.keys) if (timesOptions[key]) numFilter += 1;
 
   return numFilter;
 }
