87d8cf71a9bb42563f593b08cc30b78e08273cda
Phan Quân <qmp23@drexel.edu>
2020-05-01 02:12:27 -0400

    Add parser for calories and combine getIngredients + getInstruction into one API call and add Search Profile toggle successful

--- homechef/lib/widgets/search_widgets/search_bar.dart
+++ homechef/lib/widgets/search_widgets/search_bar.dart
@@ -2,45 +2,46 @@ import 'dart:convert';
 
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:homechef/BLoC/bloc_filter_noti.dart';
+import 'package:homechef/BLoC/bloc_provider.dart';
 import 'package:homechef/models/cuisine_model.dart';
 import 'package:homechef/models/diet_model.dart';
+import 'package:homechef/models/diets_model.dart';
 import 'package:homechef/models/nutrients/nutrients_list_model.dart';
 import 'package:homechef/models/recipe_model.dart';
+import 'package:homechef/models/search_profile_model.dart';
 import 'package:homechef/models/time_model.dart';
 import 'package:homechef/screens/recipe_screen.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:homechef/widgets/rating_stars.dart';
+import 'package:homechef/widgets/search_widgets/filter_pages/count_active_filter.dart';
 import 'package:http/http.dart' as http;
 
 import '../flappy_search_bar-1.7.2-modified/lib/flappy_search_bar.dart';
 import '../flappy_search_bar-1.7.2-modified/lib/search_bar_style.dart';
 
 class SearchBarWidget extends StatefulWidget {
-
   final SearchBarController<Recipe> searchController;
-  SearchBarWidget({
-    @required this.searchController});
+  SearchBarWidget({@required this.searchController});
 
   @override
   _SearchBarWidgetState createState() => _SearchBarWidgetState();
 }
 
-class _SearchBarWidgetState extends State<SearchBarWidget> with TickerProviderStateMixin{
-
+class _SearchBarWidgetState extends State<SearchBarWidget>
+    with TickerProviderStateMixin {
   Future<String> getFileData(String path) async {
     return await rootBundle.loadString(path);
   }
-  
+
+  bool _personallBool = false;
   bool filterUp = false;
-  TabController _tabController;
 
   void initState() {
     super.initState();
-    _tabController = new TabController(length: 2, vsync: this);
   }
-  
-  Future<List<Recipe>> search(String text) async {
 
+  Future<List<Recipe>> search(String text) async {
     String _diet = '';
     String _cuisine = '';
     String _time = '';
@@ -49,7 +50,6 @@ class _SearchBarWidgetState extends State<SearchBarWidget> with TickerProviderSt
     Map<String, String> _headers;
 
     List<Recipe> apiResult = [];
-    List<Recipe> foundResult = [];
 
     if (dietOptions.containsValue(true)) {
       for (String key in dietOptions.keys) {
@@ -69,7 +69,6 @@ class _SearchBarWidgetState extends State<SearchBarWidget> with TickerProviderSt
     }
 
     _cuisine = _cuisine.substring(0, _cuisine.length - 1);
-    
 
     if (timesOptions.containsValue(true)) {
       _time = '&maxReadyTime=';
@@ -84,46 +83,52 @@ class _SearchBarWidgetState extends State<SearchBarWidget> with TickerProviderSt
     String baseURL = 'https://api.spoonacular.com';
     // String baseURL = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com';
 
-    String searchURL = baseURL + '/recipes/complexSearch?query=' + text + 
-      _cuisine + _diet + _time +
-      "&addRecipeInformation=true&addRecipeNutrition=true" +
-      '&number=4&apiKey=' + apiKey;
+    String searchURL = baseURL +
+        '/recipes/complexSearch?query=' +
+        text +
+        _cuisine +
+        _diet +
+        _time +
+        "&addRecipeInformation=true&addRecipeNutrition=true" +
+        '&number=1&apiKey=' +
+        apiKey;
 
     if (searchURL.contains('rapidapi')) {
-       _headers = {
+      _headers = {
         "content-type": "application/json",
-        "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+        "x-rapidapi-host":
+            "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
         "x-rapidapi-key": apiKey,
       };
     }
-    final searchResult = await http.get(searchURL, headers: searchURL.contains('rapidapi') ? _headers : null);
+    final searchResult = await http.get(searchURL,
+        headers: searchURL.contains('rapidapi') ? _headers : null);
 
     print(searchURL);
 
     if (searchResult.statusCode == 200) {
-
       final Map<String, dynamic> result = json.decode(searchResult.body);
 
       for (Map<String, dynamic> obj in result['results']) {
-
         Recipe newRecipe = new Recipe(
           id: obj['id'].toString(),
-          imageUrl: obj['image'] ,
+          imageUrl: obj['image'],
           name: obj['title'],
           cookTime: obj['readyInMinutes'],
           servings: obj['servings'],
         );
 
         newRecipe.ingredients = getIngredientsOffline(obj['nutrition']);
-        newRecipe.instruction = getInstructionsOffline(obj['analyzedInstructions']);
+        newRecipe.instruction =
+            getInstructionsOffline(obj['analyzedInstructions']);
 
-        newRecipe.calories = NutrientList.fromJson(obj['nutrition']).getCalories();
-        apiResult.add(
-          newRecipe
-        );
+        newRecipe.calories =
+            NutrientList.fromJson(obj['nutrition']).getCalories();
+        apiResult.add(newRecipe);
       }
     } else {
-      print('Failed to load Recipe. Resp Code: ' + searchResult.statusCode.toString());
+      print('Failed to load Recipe. Resp Code: ' +
+          searchResult.statusCode.toString());
     }
 
     return apiResult;
@@ -132,16 +137,13 @@ class _SearchBarWidgetState extends State<SearchBarWidget> with TickerProviderSt
   Widget buildSearchRecipeResult(Recipe recipe) {
     return GestureDetector(
       onTap: () => Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (_) => RecipePage(
-            recipe: recipe,
-          )
-        )
-      ),
+          context,
+          MaterialPageRoute(
+              builder: (_) => RecipePage(
+                    recipe: recipe,
+                  ))),
       child: Stack(
         children: <Widget>[
-
           Container(
             margin: EdgeInsets.fromLTRB(20.0, 5.0, 20.0, 5.0),
             height: 170.0,
@@ -163,14 +165,12 @@ class _SearchBarWidgetState extends State<SearchBarWidget> with TickerProviderSt
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-
                   Expanded(
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.start,
                       crossAxisAlignment: CrossAxisAlignment.start,
                       mainAxisSize: MainAxisSize.max,
                       children: <Widget>[
-                        
                         SizedBox(height: 20.0),
                         AutoSizeText(
                           recipe.name,
@@ -188,11 +188,10 @@ class _SearchBarWidgetState extends State<SearchBarWidget> with TickerProviderSt
                             color: Colors.grey,
                           ),
                         ),
-                        
                       ],
                     ),
                   ),
-                  
+
                   SizedBox(height: 10.0),
                   RatingStars(rating: recipe.rate),
                   // SizedBox(height: 10.0),
@@ -200,11 +199,10 @@ class _SearchBarWidgetState extends State<SearchBarWidget> with TickerProviderSt
               ),
             ),
           ),
-
           Positioned(
             left: 30.0,
             top: 15.0,
-            bottom: 15.0,
+            bottom: 35.0,
             child: ClipRRect(
               borderRadius: BorderRadius.circular(20.0),
               child: Image.network(
@@ -220,68 +218,85 @@ class _SearchBarWidgetState extends State<SearchBarWidget> with TickerProviderSt
   }
 
   Widget buildHeaderRow() {
-      return Container(
-        decoration: BoxDecoration(
-          color: Colors.grey[50],
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey[200],
-              blurRadius: 5.0,
-              spreadRadius: 2.0,
-              offset: Offset(0.0, 7.0)
-            )
-          ]
-        ),
-        child: TabBar(
-          controller: _tabController,
-          tabs: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(left: 20.0, right: 20.0, top: 5.0, bottom: 5.0),
-              child: FlatButton.icon(
-                disabledTextColor: Colors.grey[700],
-                onPressed: null, icon: Icon(Icons.filter_list), label: Text("Custom"),),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 20.0, right: 20.0, top: 5.0, bottom: 5.0),
-              child: FlatButton.icon(
-                disabledTextColor: Colors.grey[700],
-                onPressed: null, icon: Icon(Icons.person_outline), label: Text("Profile"),),
-            ),
-          ],
-          indicator: UnderlineTabIndicator(
-            borderSide: BorderSide(color: Colors.red[400], width: 2.0),
-            insets: EdgeInsets.symmetric(horizontal: 70.0)),
-          labelColor: Colors.grey[800],
-          unselectedLabelColor: Colors.grey[600],
-        ),
-      );
-    }
-
-    Align cancelWidget = Align(
-      alignment: Alignment.centerRight,
-      child: Padding(
-        padding: const EdgeInsets.only(left: 8.0),
-        child: Container(
-          height: 50.0,
-          width: 50.0,
-          decoration: BoxDecoration(
-            color: Colors.white70,
-            borderRadius: BorderRadius.circular(30.0)
+    return Container(
+      height: 50.0,
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(color: Colors.grey[50], boxShadow: [
+        BoxShadow(
+            color: Colors.grey[200],
+            blurRadius: 5.0,
+            spreadRadius: 2.0,
+            offset: Offset(0.0, 7.0))
+      ]),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Icon(
+                Icons.person_outline,
+                color: Colors.amber[400],
+              ),
+              SizedBox(width: 5.0),
+              Text(
+                "Search Profile",
+                style: TextStyle(fontWeight: FontWeight.w600, color: Colors.grey[800]),
+              )
+            ],
           ),
-          child: Center(child: 
-              IconButton(
-                onPressed: () {
-                  // widget.searchController.clear();
-                  // FocusScope.of(context).unfocus();
-                },
-                icon: Icon(Icons.cancel))
-            ),
-        ),
+          SizedBox(
+            width: 10.0,
+          ),
+          StreamBuilder<Map<String, dynamic>>(
+              stream: BlocProvider.of<NotiFilterBloc>(context).notiStream,
+              initialData: {'numNoti': 0, 'saveProfileSelect': false},
+              builder: (context, snapshot) {
+                if (snapshot.hasData &&
+                    snapshot.data.containsKey('saveProfileSelect'))
+                  _personallBool = snapshot.data['saveProfileSelect'];
+
+                return Switch(
+                  activeColor: Colors.orange,
+                  value: _personallBool,
+                  onChanged: (value) {
+                    setState(() {
+                      _personallBool = value;
+                    });
+
+                    Diets dietsController = Diets();
+
+                    if (value) {
+                      getDietsCopy();
+
+                      dietsController.setAllFalse();
+
+                      dietOptions[encodeDietName(SearchProfile().getSaveDiet)] =
+                          true;
+                    } else {
+                      if (dietOptionsCopy != null)
+                        dietOptions.updateAll((key, val) {
+                          return dietOptionsCopy[key];
+                        });
+                    }
+
+                    final notiFilterBloc =
+                        BlocProvider.of<NotiFilterBloc>(context);
+                    notiFilterBloc.modifyNoti(
+                        {'numNoti': numFilters(), 'saveProfileSelect': value});
+
+                    widget.searchController.triggerSearch();
+                  },
+                );
+              }),
+        ],
       ),
     );
+  }
+
   @override
   Widget build(BuildContext context) {
-
     Align cancelWidget = Align(
       alignment: Alignment.centerRight,
       child: Padding(
@@ -290,23 +305,19 @@ class _SearchBarWidgetState extends State<SearchBarWidget> with TickerProviderSt
           height: 50.0,
           width: 50.0,
           decoration: BoxDecoration(
-            color: Colors.white70,
-            borderRadius: BorderRadius.circular(30.0)
-          ),
-          child: Center(child: 
-              IconButton(
-                onPressed: () {
-                  widget.searchController.clear();
-                  FocusScope.of(context).unfocus();
-                },
-                icon: Icon(Icons.cancel))
-            ),
+              color: Colors.white70, borderRadius: BorderRadius.circular(30.0)),
+          child: Center(
+              child: IconButton(
+                  onPressed: () {
+                    widget.searchController.clear();
+                    FocusScope.of(context).unfocus();
+                  },
+                  icon: Icon(Icons.cancel))),
         ),
       ),
     );
 
     return SearchBar<Recipe>(
-
       searchBarStyle: SearchBarStyle(
         backgroundColor: Colors.white,
         borderRadius: BorderRadius.circular(30.0),
@@ -324,22 +335,25 @@ class _SearchBarWidgetState extends State<SearchBarWidget> with TickerProviderSt
       // headerPadding: EdgeInsets.all(5.0),
       searchBarPadding: EdgeInsets.only(left: 20.0, right: 20.0),
       searchBarController: widget.searchController,
-      
+
       header: buildHeaderRow(),
 
       cancellationWidget: cancelWidget,
-      onError: (Error error) { return Text(error.toString());},
+      onError: (Error error) {
+        return Text(error.toString());
+      },
       onCancelled: () {
         print('Cancel Search');
       },
 
       debounceDuration: Duration(milliseconds: 1500),
-      minimumChars:2,
+      minimumChars: 2,
       onSearch: search,
       onItemFound: (Recipe recipe, int index) {
-
         return Container(
-          child: buildSearchRecipeResult(recipe)
+          height: 200.0,
+          width: 200.0,
+          child: buildSearchRecipeResult(recipe),
         );
       },
     );
