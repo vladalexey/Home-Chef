2997d571a452a62dd93bf63eeafe029c2547648c
Phan Quân <qmp23@drexel.edu>
2020-05-01 02:19:30 -0400

    Fix badge number of filters on bottom sheet

--- homechef/lib/widgets/search_widgets/bottom_sheet_header.dart
+++ homechef/lib/widgets/search_widgets/bottom_sheet_header.dart
@@ -1,6 +1,7 @@
 import 'package:badges/badges.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:homechef/BLoC/bloc_filter_noti.dart';
 import 'package:homechef/BLoC/bloc_provider.dart';
@@ -28,32 +29,67 @@ class BottomSheetHeader extends StatelessWidget {
       child: Stack(
         children: <Widget>[
 
-          StreamBuilder<int>(
+          Align(
+            alignment: Alignment.topCenter,
+            child: Container(
+              width: 200.0,
+              child: Divider(
+                  color: Colors.grey[200],
+                  indent: 20.0,
+                  endIndent: 20.0,
+                  thickness: 3.0,
+                ),
+            ),
+          ),
+
+          Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                // SizedBox(width: 40.0,),
+                Icon(Ionicons.ios_options, size: 25.0, color: Colors.amber[400],),
+                SizedBox(width: 10.0,),
+                Text("Filters",
+                  style: GoogleFonts.raleway(
+                    textStyle: TextStyle(
+                      color: Colors.grey[800],
+                      fontSize: 18.0,
+                      letterSpacing: 0.5,
+                      fontWeight: FontWeight.w700),
+                  ),
+                ),
+              ],
+            )
+          ),
+
+          StreamBuilder<Map<String, dynamic>>(
             stream: BlocProvider.of<NotiFilterBloc>(context).notiStream,
             builder: (context, snapshot) {
-              print(snapshot.data);
-              if (snapshot.hasData && snapshot.data != 0) {
-                return Positioned(
-                  right: 20.0,
-                  top: 0.0,
-                  child: Badge(
-                    borderRadius: 1.0,
-                    badgeContent: Padding(
-                      padding: const EdgeInsets.all(3.0),
-                      child: Text(snapshot.data.toString(), style: TextStyle(color: Colors.white),),
+
+              if (snapshot.hasData && snapshot.data['numNoti'] != 0) {
+                return Center(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 90.0, bottom: 20.0),
+                    child: Badge(
+                      borderRadius: 1.0,
+                      badgeContent: Padding(
+                        padding: const EdgeInsets.all(3.0),
+                        child: Text(snapshot.data['numNoti'].toString(), style: TextStyle(color: Colors.white),),
+                      ),
                     ),
                   ),
                 );
               } else {
                 return (numFilters() != 0) 
-                ? Positioned(
-                  right: 20.0,
-                  top: 0.0,
-                  child: Badge(
-                    borderRadius: 1.0,
-                    badgeContent: Padding(
-                      padding: const EdgeInsets.all(3.0),
-                      child: Text( numFilters().toString(), style: TextStyle(color: Colors.white),),
+                ? Center(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 90.0, bottom: 20.0),
+                    child: Badge(
+                      borderRadius: 1.0,
+                      badgeContent: Padding(
+                        padding: const EdgeInsets.all(3.0),
+                        child: Text(numFilters().toString(), style: TextStyle(color: Colors.white),),
+                      ),
                     ),
                   ),
                 )
@@ -62,18 +98,7 @@ class BottomSheetHeader extends StatelessWidget {
               
             }
           ),
-
-          Center(
-            child: Text("Filters",
-              style: GoogleFonts.raleway(
-                textStyle: TextStyle(
-                  color: Colors.grey[800],
-                  fontSize: 18.0,
-                  letterSpacing: 0.5,
-                  fontWeight: FontWeight.w700),
-              ),
-            ),
-          ),
+          
         ],
       ),
     );
